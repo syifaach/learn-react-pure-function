ff91ab2cf4d8b6d703f13bdf050aab14da0d6600
Syifa Chaerunnisa <syifachrnnsas@gmail.com>
2026-03-28 23:15:30 +0700

    simple debounce search filter

---
+++ app/search-filter/page.tsx
@@ -0,0 +1,65 @@
+"use client"
+
+import React, { useEffect, useState } from "react"
+
+const SearchFilter = () => {
+    const [data, setData] = useState<any[]>([])
+    const [searchValue, setSearchValue] = useState<string>('')
+    const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        let controller: AbortController
+        const debounceInput = setTimeout(() => {
+            controller = new AbortController()
+            setLoading(true)
+
+            fetch(`https://restcountries.com/v3.1/name/${searchValue}`, { signal: controller.signal })
+                .then(async res => {
+                    if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+
+                    const text = await res.text(); // read as plain text first
+                    return JSON.parse(text); // then parse manually
+                })
+                .then(res => {
+                    setError(null)
+                    setData(res)
+                })
+                .catch(err => {
+                    console.log('error name:', err.name, 'message:', err.message); // 👈
+                    if (err.name === 'AbortError') {
+                        setError(null)
+                        return
+                    };
+                    setError(err);
+                })
+                .finally(() => setLoading(false))
+        }, 500)
+
+        return () => {
+            clearTimeout(debounceInput)
+            controller?.abort()
+        }
+    }, [searchValue])
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target
+        setSearchValue(value)
+    }
+
+    console.log('data', data, 'err', error)
+
+    return (
+        <div className="flex flex-col gap-2">
+            <input className="border" placeholder="search" name="search" id="search" value={searchValue} onChange={onChange} />
+            {loading ? <label>Loading...</label>
+                : error ? <label>Something went wrong!</label>
+                    : data.map(val => (
+                        <label key={val.name.common}>{val.name.common}</label>
+                    ))
+            }
+        </div>
+    )
+}
+
+export default SearchFilter
